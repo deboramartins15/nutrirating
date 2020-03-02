a683c097b256160aeb04579b67b8d1b8b3592c06
deboramartins15 <deehmartin@gmail.com>
2020-03-02 19:18:12 -0300

    Ajustes na landing page

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React from "react";
 
-import "./Landing.css"
-import bg from "../../assets/bg_landing.png"
-import logo from "../../assets/logo_desktop.png"
+import "./Landing.css";
+import bg from "../../assets/bg_landing.png";
+import logo from "../../assets/logo_desktop.png";
 
-function Landing(props){
-    return(
-        <>
-            <div className="container">
-                <img src={bg} alt="bg"></img>
-                <div className="header">
-                    <img src={logo} alt="logo"></img>
-                    <button className="btnEntrar">Entrar</button>
-                </div>                
-                <div className="content"></div>
-            </div>
-        </>
-    )
+function Landing(props) {
+  return (
+    <>
+      <div className="container">
+        <img src={bg} alt="bg"></img>
+        <div className="header above">
+          <img src={logo} alt="logo"></img>
+          <button className="btnEntrar">Entrar</button>
+        </div>
+        <div className="content above">
+          <div className="title">
+            <h2 id="title-1">Ajudando</h2>
+            <h2 id="title-2">você</h2>
+          </div>
+          <div className="subtitle">
+            <h5>a avaliar quem importa</h5>
+          </div>
+          <div>
+            <button className="btnComece">Comece já</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Landing;
